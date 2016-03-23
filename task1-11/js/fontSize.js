019aeb22481781531f5bfa31b2bfd5dc992aec80
NotGeekAtAll <254489973@qq.com>
2016-03-23 15:29:40 +0800

    移动页面

--- task1-11/js/fontSize.js
+++ task1-11/js/fontSize.js
@@ -1,1 +1,9 @@
-there is no conflict now
+//动态设置html的字体大小，默认html{font-size:50px}
+(function () {
+    document.addEventListener('DOMContentLoaded', function () {
+        var html = document.documentElement;
+        var windowWidth = html.clientWidth; html.style.fontSize = windowWidth / 7.5 + 'px';
+        //等价于html.style.fontSize = windowWidth / 720 * 100 + 'px';
+        //同理可设6.4为640对应的情况
+    }, false);
+})();
